4d4fd3c08bd0aa91c67342ab26a1733b5f2df1ed
donghyukkil <asterism90@gmail.com>
2024-02-10 22:23:11 +0900

    feat: 정적 페이지 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/widgets/button.dart';
+import 'package:toonflix/widgets/currency_card.dart';
 
 class Player {
   String? name;
@@ -25,7 +26,7 @@ class App extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const SizedBox(
-                  height: 80,
+                  height: 70,
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.end,
@@ -50,7 +51,7 @@ class App extends StatelessWidget {
                     )
                   ],
                 ),
-                const SizedBox(height: 120),
+                const SizedBox(height: 30),
                 const Text("Total Balance",
                     style: TextStyle(color: Colors.white, fontSize: 22)),
                 const SizedBox(
@@ -66,35 +67,73 @@ class App extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Button(
-                        text: "Transfer",
-                        bgColor: Color(0xFFF1B33B),
-                        textColor: Colors.black),
-                    Mybutton()
+                      text: "Transfer",
+                      bgColor: Color(0xFFF1B33B),
+                      textColor: Colors.black,
+                    ),
+                    Button(
+                      text: "Request",
+                      bgColor: Color(0xFF1F2123),
+                      textColor: Colors.white,
+                    ),
                   ],
                 ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Wallets",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 36,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      "View All",
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.8),
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const CurrencyCard(
+                  name: "Euro",
+                  code: "EUR",
+                  amount: "6 428",
+                  icon: Icons.euro_rounded,
+                  isInverted: false,
+                ),
+                Transform.translate(
+                  offset: const Offset(0, -20),
+                  child: const CurrencyCard(
+                    name: "Bitcoin",
+                    code: "BTC",
+                    amount: "9 785",
+                    icon: Icons.currency_bitcoin,
+                    isInverted: true,
+                  ),
+                ),
+                Transform.translate(
+                  offset: const Offset(0, -40),
+                  child: const CurrencyCard(
+                    name: "Dollar",
+                    code: "USD",
+                    amount: "428",
+                    icon: Icons.attach_money_outlined,
+                    isInverted: false,
+                  ),
+                )
               ],
             ),
           )),
-    );
-  }
-}
-
-class Mybutton extends StatelessWidget {
-  const Mybutton({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: const Color(0xFF1F2123),
-          borderRadius: BorderRadius.circular(45)),
-      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-      child: const Text(
-        "Request",
-        style: TextStyle(fontSize: 20, color: Colors.white),
-      ),
     );
   }
 }
