a7ce7a405f6d65ee8fd72c83ba8e037bf082e716
donghyukkil <asterism90@gmail.com>
2024-02-11 11:16:09 +0900

    feat: stateWidget 추가

---
+++ lib/widgets/stateWidget.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class Player {
+  String? name;
+
+  Player({required this.name});
+}
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  // int counter = 0;
+
+  List<int> numbers = [];
+
+  void onClicked() {
+    setState(() {
+      // counter = counter + 1;
+      numbers.add(numbers.length);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: const Color(0xFFF4EDDB),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Click Count",
+                style: TextStyle(
+                  fontSize: 30,
+                )),
+            // Text('$counter', style: const TextStyle(fontSize: 30)),
+
+            for (var n in numbers) Text("$n"),
+            IconButton(
+                iconSize: 40,
+                onPressed: onClicked,
+                icon: const Icon(Icons.add_box_rounded))
+          ],
+        ),
+      ),
+    ));
+  }
+}
